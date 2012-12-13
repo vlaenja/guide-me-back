60f7bcc55a39cfa0a75b21856d4f9fd5ee15970c
jan.vanvlaenderen@gmail.com <jan.vanvlaenderen@gmail.com@a2f3f0e8-d0e7-1f16-9a75-f6d63c473449>
2012-12-13 10:32:30 +0000

    Complete versie voor proof of concept

---
+++ tags/version.0.1.1/AndroidUtils/src/be/jvvlaend/utils/android/compass/AverageCompassData.java
@@ -0,0 +1,33 @@
+package be.jvvlaend.utils.android.compass;
+
+import java.util.ArrayList;
+
+public class AverageCompassData {
+	private static final int CAPACITY = 50;
+	private ArrayList<CompassData> dataList = new ArrayList<CompassData>(CAPACITY);
+
+	public void add(CompassData compassData) {
+		dataList.add(compassData);
+		if (dataList.size() > CAPACITY) {
+			dataList.remove(0);
+		}
+	}
+
+	public CompassData getAverage() {
+		float x = 0, y = 0, z = 0;
+		int numberOfElements = dataList.size();
+		if (numberOfElements == 0) {
+			return null;
+		}
+		CompassData avgData = new CompassData(dataList.get(0).getSensorEvent());
+		for (CompassData data : dataList) {
+			x += data.getX();
+			y += data.getY();
+			z += data.getZ();
+		}
+		avgData.setX(x / numberOfElements);
+		avgData.setY(y / numberOfElements);
+		avgData.setZ(z / numberOfElements);
+		return avgData;
+	}
+}
