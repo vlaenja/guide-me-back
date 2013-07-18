f8827f85364d8ce5636c51bc292bc8e71613a333
jan.vanvlaenderen@gmail.com <jan.vanvlaenderen@gmail.com@a2f3f0e8-d0e7-1f16-9a75-f6d63c473449>
2013-07-18 04:03:17 +0000

    Version 1.0 on market

---
+++ tags/version.1.0/src/be/jvvlaend/utils/android/utils/Utils.java
@@ -0,0 +1,33 @@
+package be.jvvlaend.utils.android.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+
+	public static float afrondenTotCijfersNaKomma(float value, int cijfersNaKomma) {
+		return Float.valueOf(value * 10f).intValue() / 10f;
+	}
+
+	public static double afrondenTotCijfersNaKomma(double value, int cijfersNaKomma) {
+		return Double.valueOf(value * 10).intValue() / 10;
+	}
+
+	public static String formatDate(long timeStamp) {
+		Date date = new Date();
+		date.setTime(timeStamp);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		return dateFormat.format(date);
+	}
+
+	public static String formatDistanceToDestinationWithUnits(float distanceTo) {
+		int distance = Float.valueOf(distanceTo).intValue();
+		if (distance < 10000) {
+			return String.valueOf(distance) + " m";
+		} else {
+			return String.valueOf(Utils.afrondenTotCijfersNaKomma(distance / 1000f, 2)) + " Km";
+		}
+
+	}
+
+}
